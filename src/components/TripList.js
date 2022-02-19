e21c43d95350243c35697ad1d3145e6e6f9a30e0
surya vishakan <suryavishakan050@gmail.com>
2022-02-19 08:09:13 +0530

    changes made to TripList.js file

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,23 +1,29 @@
 import React, { useState } from "react";
+
+// Import our custom Hook
 import { useFetch } from "../hooks/useFetch";
 
 // Import CSS
 import "./TripList.css";
 
 const TripList = () => {
+  // state for URL change
   const [url, setUrl] = useState("http://localhost:3000/trips");
 
+  // custom hook
   const { data: trips, error } = useFetch(url);
 
   return (
     <div className="trip-list">
       {/* {isLoading && <div>Loading trips....</div>} */}
+      {/* if there is an error display the 404 error message */}
       {error && (
         <div>
           <h2>404 Error</h2>
           <h4>{error}</h4>
         </div>
       )}
+      {/*  if the trips array  is loaded, then display the trip list */}
       {trips && (
         <>
           <h2>Trip List</h2>
@@ -29,6 +35,7 @@ const TripList = () => {
               </li>
             ))}
           </ul>
+          {/* Filter Buttons */}
           <div className="filters">
             <button onClick={() => setUrl("http://localhost:3000/trips")}>
               All Trips
